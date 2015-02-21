c5e28f85db880ba9a1ee024196ab7246bcc1c574
Togo Harrian <togo.harrian@gmail.com>
2015-02-21 09:25:13 +0100

    Master config class

---
+++ src/Config.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is a part of the Calmacil/rpgl project
+ *
+ * @package     Config
+ * @author      Calmacil <thomas.lenoel@gmail.com>
+ * @version     0.1
+ * @copyright   (C) 2015 Calmacil <thomas.lenoel@gmail.com>
+ * @license     GPL-3.0
+ */
+
+namespace RPGL;
+
+use \RPGL\Utils\JsonReader;
+
+class Config extends JsonReader
+{
+    /**
+     * Instance holder for singleton
+     *
+     * @var Config $_instance
+     */
+    private $_instance = null;
+
+    /**
+     * Loads and read given config file
+     *
+     * @param string $path      The path of the JSON file
+     */
+    private function __construct($path)
+    {
+        parent::__construct($path);
+    }
+
+    /**
+     * Implements the singleton
+     *
+     * @param string $path      The path of the JSON file
+     * @return Config
+     */
+    public static function getInstance($path)
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Config($path);
+        }
+        return self::$_instance;
+    }
+}
